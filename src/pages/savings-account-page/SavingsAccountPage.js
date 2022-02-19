4a2c82e4c010a49dab05198558d4fec14878b6c7
Lina Dang-Nguyen <lina_dang-nguyen@sfu.ca>
2022-02-19 14:52:02 -0800

    savings account images

--- src/pages/savings-account-page/SavingsAccountPage.js
+++ src/pages/savings-account-page/SavingsAccountPage.js
@@ -1,13 +1,31 @@
 import "./SavingsAccountPage.css";
 import "../../App.css";
 import React, { forwardRef, useRef } from "react";
+import Car from "../../assets/car.png";
+import House from "../../assets/house.png";
+import Cabin from "../../assets/cabin.png";
 
 export const SavingsAccountPage = forwardRef(({ onBackClick }, ref) => {
     return (
         <div className="container">
             <section>
-                <h3 ref={ref}>✨what are you manifesting ?✨</h3>
-                <h5 className="big-text-body">yeet</h5>
+                <h3 className="title" ref={ref}>
+                    ✨what are you manifesting ?✨
+                </h3>
+                <div className="images-wrapper">
+                    <div className="image-container">
+                        <img src={Car} width="504" height="464" alt="Logo" />
+                        <h5>Car</h5>
+                    </div>
+                    <div className="image-container">
+                        <img width="504" height="464" src={House} alt="Logo" />
+                        <h5>House</h5>
+                    </div>
+                    <div className="image-container">
+                        <img src={Cabin} width="504" height="464" alt="Logo" />
+                        <h5>Cabin</h5>
+                    </div>
+                </div>
             </section>
         </div>
     );
